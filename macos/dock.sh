6060e2b1b63c4d1bd49f67d63afeb43be4e16f32
Andrew Baughman <abaughman@brotherhoodmutual.com>
2025-03-27 13:53:01 -0400

    Add my work dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -2,6 +2,20 @@
 
 dockutil --no-restart --remove all
 
-# dockutil --no-restart --add "/Applications/AppName.app"
+dockutil --no-restart --add "$HOME/Applications/Outlook.app"
+dockutil --no-restart --add "/Applications/Microsoft Teams.app"
+dockutil --no-restart --add "/Applications/Microsoft To Do.app"
+dockutil --no-restart --add "/Applications/Obsidian.app"
+dockutil --no-restart --add "/Applications/Notes.app"
+dockutil --no-restart --add "/System/Applications/Launchpad.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/Applications/Postman.app"
+dockutil --no-restart --add "/Applications/One Markdown.app"
+dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
+dockutil --no-restart --add "/Applications/GitUp.app"
+dockutil --no-restart --add "/Applications/Fork.app"
+dockutil --no-restart --add "/Applications/ChatGPT.app"
 
 killall Dock
